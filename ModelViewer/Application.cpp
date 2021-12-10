af4da3447d0881cd99c7f46bfb696b845ad398be
realn <realnoname@coderulers.info>
2021-12-10 18:04:00 +0100

    Added simple model loading to modelviewer.

--- ModelViewer/Application.cpp
+++ ModelViewer/Application.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <CBCore/StringConvert.h>
 #include <CBSDL/Events.h>
@@ -38,29 +39,24 @@ namespace mdlview {
   }
 
   bool Application::init() {
-    cb::sdl::GLAttributeMapT attribs = {
-      {cb::sdl::GLAttribute::BUFFER_SIZE, 32},
-      {cb::sdl::GLAttribute::DEPTH_SIZE, 24},
-      {cb::sdl::GLAttribute::STENCIL_SIZE, 8},
-      {cb::sdl::GLAttribute::DOUBLEBUFFER, 1}
-    };
+    if (!initBase())
+      return false;
+    if (!initCore())
+      return false;
 
-    cb::sdl::setContextCreationAttributes(attribs);
+    scriptParser->addFunc(L"loadModel", [this](const core::StrArgList& args) {
+      auto filename = args.getArgUtf8(0);
+      if (filename.empty())
+        return;
 
-    window = std::make_unique<cb::sdl::Window>(L"ModelViewer", cb::sdl::Window::PosCentered, glm::uvec2{ 1200, 800 }, cb::sdl::WindowFlag::OPENGL);
-    window->show();
 
-    glContext = std::make_unique<cb::sdl::GLContext>(*window);
+      model = std::make_shared<gfx::Model>();
+      model->load(*texRepo, "Data/Models/" + filename);
+                          });
 
-    cb::gl::initextensions();
-
-    texRepo = std::make_unique<gfx::TextureRepository>("Data/Textures/");
-
-    textPrinter = std::make_shared<gui::TextPrinter>(texRepo->Get("Font.tga"));
-    
     modelMenu = std::make_shared<gui::Menu>("modelMenu", textPrinter->getFontInfo());
     modelMenu->setSize({ 1200, 800 });
-    mainMenu = std::make_shared<gui::MenuMain>(getAspectRatio(window->getSize()));
+    mainMenu = std::make_shared<gui::MenuMain>(getAspectRatio(window->getSize()), scriptParser);
     mainMenu->addMenu(modelMenu);
 
     {
@@ -76,8 +72,7 @@ namespace mdlview {
     }
 
     mainMenu->push("modelMenu");
-
-    events.addObserver(mainMenu);
+    events->addObserver(mainMenu);
 
     input.addKeyMapping(cb::sdl::ScanCode::UP, L"gui_move_up");
     input.addKeyMapping(cb::sdl::ScanCode::DOWN, L"gui_move_down");
@@ -86,6 +81,36 @@ namespace mdlview {
     input.addMouseMotionMapping(event::InputMapper::Axis::X, L"gui_pointer_x");
     input.addMouseMotionMapping(event::InputMapper::Axis::Y, L"gui_pointer_y");
     input.addMouseMapping(cb::sdl::button::LEFT, L"gui_enter");
+
+    return true;
+  }
+
+  bool Application::initBase() {
+    cb::sdl::GLAttributeMapT attribs = {
+      {cb::sdl::GLAttribute::BUFFER_SIZE, 32},
+      {cb::sdl::GLAttribute::DEPTH_SIZE, 24},
+      {cb::sdl::GLAttribute::STENCIL_SIZE, 8},
+      {cb::sdl::GLAttribute::DOUBLEBUFFER, 1}
+    };
+
+    cb::sdl::setContextCreationAttributes(attribs);
+
+    window = std::make_unique<cb::sdl::Window>(L"ModelViewer", cb::sdl::Window::PosCentered, glm::uvec2{ 1200, 800 }, cb::sdl::WindowFlag::OPENGL);
+    window->show();
+
+    glContext = std::make_unique<cb::sdl::GLContext>(*window);
+
+    cb::gl::initextensions();
+    return true;
+  }
+
+  bool Application::initCore() {
+    events = std::make_shared<event::Manager>();
+
+    scriptParser = std::make_shared<logic::ScriptParser>(events);
+
+    texRepo = std::make_unique<gfx::TextureRepository>("Data/Textures/");
+    textPrinter = std::make_shared<gui::TextPrinter>(texRepo->Get("Font.tga"));
 
     return true;
   }
@@ -103,12 +128,12 @@ namespace mdlview {
           }
         }
 
-        input.executeEvent(events, event, window->getSize());
+        input.executeEvent(*events, event, window->getSize());
       }
 
-      events.processEvents();
+      events->processEvents();
 
-      update(1.0f/60.0f);
+      update(1.0f / 60.0f);
 
       render();
 
@@ -123,8 +148,22 @@ namespace mdlview {
   void Application::render() {
     cb::gl::clearColor({ 1.0f, 0.5f, 0.5f, 1.0f });
 
+    if (model) {
+      auto mat = glm::perspective(glm::radians(90.0f), getAspectRatio(window->getSize()), 0.1f, 100.0f);
+
+      glMatrixMode(GL_PROJECTION);
+      glLoadMatrixf(glm::value_ptr(mat));
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+
+      mat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
+      glLoadMatrixf(glm::value_ptr(mat));
+
+      model->render(0);
+    }
+
     auto ctx = modelMenu->makeRender(*textPrinter);
-    
+
     glMatrixMode(GL_PROJECTION);
     glLoadMatrixf(glm::value_ptr(ctx.getProjectionMatrix()));
     glMatrixMode(GL_MODELVIEW);
--- ModelViewer/Application.h
+++ ModelViewer/Application.h
@@ -9,7 +9,10 @@
 #include <event_InputMapper.h>
 #include <event_Manager.h>
 
+#include <logic_ScriptParser.h>
+
 #include <gfx_TextureRepository.h>
+#include <gfx_Model.h>
 
 #include <GUITextPrinter.h>
 
@@ -27,23 +30,28 @@ namespace mdlview {
 
   private:
     bool init();
+    bool initBase();
+    bool initCore();
     void mainLoop();
 
     void update(float timeDelta);
     void render();
 
     bool run = true;
-    event::Manager events;
     event::InputMapper input;
 
     std::unique_ptr<cb::sdl::System> system;
     std::unique_ptr<cb::sdl::Window> window;
     std::unique_ptr<cb::sdl::GLContext> glContext;
 
+    std::shared_ptr<event::Manager> events;
+    std::shared_ptr<logic::ScriptParser> scriptParser;
+
     std::unique_ptr<gfx::TextureRepository> texRepo;
     std::shared_ptr<gui::TextPrinter> textPrinter;
     std::shared_ptr<gui::Menu> modelMenu;
     std::shared_ptr<gui::MenuMain> mainMenu;
 
+    std::shared_ptr<gfx::Model> model;
   };
 }
